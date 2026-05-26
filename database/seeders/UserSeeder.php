638b34cdee0858ff24204f7c2d28fafd894f0294
Wahyu Dwi Utomo <wahyuxd14@gmail.com>
2026-05-26 20:52:35 +0700

    buat seeder user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's users.
+     */
+    public function run(): void
+    {
+        $password = Hash::make('password');
+
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gereja.test',
+            ],
+            [
+                'name' => 'Ketua Panitia',
+                'email' => 'ketua.panitia@gereja.test',
+            ],
+            [
+                'name' => 'Sekertaris',
+                'email' => 'sekertaris@gereja.test',
+            ],
+            [
+                'name' => 'Jamaat',
+                'email' => 'jamaat@gereja.test',
+            ],
+        ];
+
+        foreach ($users as $user) {
+            User::updateOrCreate(
+                ['email' => $user['email']],
+                [
+                    'name' => $user['name'],
+                    'email_verified_at' => now(),
+                    'password' => $password,
+                ]
+            );
+        }
+    }
+}
